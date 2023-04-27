2f8c9d23584055f2ba3bdcc7da0e7f57e0680a60
Phat Nguyen <ntphat@tma.com.vn>
2023-04-28 01:00:58 +0700

    refs (2)4.2.CLI(参加メンバーの新規登録、上書き) : IT - update UT code

--- tests/factories.py
+++ tests/factories.py
@@ -2,17 +2,155 @@ import json
 from argparse import Namespace
 from types import SimpleNamespace
 
+user_str = '''{
+        "id": "typ46",
+        "type": "users",
+        "attributes": {
+            "uid": "36",
+            "full_name": "admin01_inst17 example.com.vn",
+            "given_name": "admin01_inst17",
+            "middle_names": "",
+            "family_name": "example.com.vn",
+            "suffix": "",
+            "date_registered": "2023-01-31T08:28:35.644862",
+            "active": true,
+            "timezone": "Etc/UTC",
+            "locale": "en_US",
+            "social": {},
+            "employment": [
+                {
+                    "title": "",
+                    "endYear": null,
+                    "ongoing": false,
+                    "endMonth": null,
+                    "startYear": null,
+                    "department": "",
+                    "startMonth": null,
+                    "institution": "TMA",
+                    "department_ja": "",
+                    "institution_ja": "TMA"
+                }
+            ],
+            "education": [],
+            "can_view_reviews": [],
+            "accepted_terms_of_service": true
+        },
+        "relationships": {
+            "nodes": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/nodes/",
+                        "meta": {}
+                    }
+                }
+            },
+            "groups": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/groups/",
+                        "meta": {}
+                    }
+                }
+            },
+            "quickfiles": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/quickfiles/",
+                        "meta": {}
+                    },
+                    "upload": {
+                        "href": "http://localhost:7777/v1/resources/uancbt93pe/providers/osfstorage/",
+                        "meta": {}
+                    },
+                    "download": {
+                        "href": "http://localhost:7777/v1/resources/uancbt93pe/providers/osfstorage/?zip=",
+                        "meta": {}
+                    }
+                }
+            },
+            "registrations": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/registrations/",
+                        "meta": {}
+                    }
+                }
+            },
+            "institutions": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/institutions/",
+                        "meta": {}
+                    },
+                    "self": {
+                        "href": "http://localhost:8000/v2/users/qm73w/relationships/institutions/",
+                        "meta": {}
+                    }
+                }
+            },
+            "preprints": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/preprints/",
+                        "meta": {}
+                    }
+                }
+            },
+            "emails": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/settings/emails/",
+                        "meta": {}
+                    }
+                }
+            },
+            "default_region": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/regions/csic/",
+                        "meta": {}
+                    }
+                },
+                "data": {
+                    "id": "csic",
+                    "type": "regions"
+                }
+            },
+            "settings": {
+                "links": {
+                    "related": {
+                        "href": "http://localhost:8000/v2/users/qm73w/settings/",
+                        "meta": {}
+                    }
+                },
+                "data": {
+                    "id": "qm73w",
+                    "type": "user-settings"
+                }
+            }
+        },
+        "links": {
+            "html": "http://localhost:5000/qm73w/",
+            "profile_image": "https://secure.gravatar.com/avatar/bca17c92257b100912db5f810514a98b?d=identicon",
+            "self": "http://localhost:8000/v2/users/qm73w/"
+        }
+    }'''
+
 
 class CommonCLIFactory(Namespace):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.config_default = {}
         self._meta = {}
         self.config_file = 'D:/config.config'
         self.is_authenticated = False
         self.user = None
         self.template = 'path-to-template'
         self.output_result_file = 'path-to-write-file'
+        self.affiliated_institutions = []
+        self.affiliated_users = []
+        self.projects = []
 
     @property
     def has_required_attributes(self):
@@ -33,15 +171,22 @@ class CommonCLIFactory(Namespace):
     def _users_me(self, ignore_error=True, verbose=True):
         pass
 
+    def _prepare_output_file(self):
+        pass
 
-class GRDMClientContributorsCreateFactory(CommonCLIFactory):
+
+class GRDMClientFactory(CommonCLIFactory):
 
     def __init__(self):
         super().__init__()
-        self.user = json.loads('{"id": "typ46"}',
-                               object_hook=lambda d: SimpleNamespace(**d))
+        self.user = json.loads(user_str, object_hook=lambda d: SimpleNamespace(**d))
+        self.verbose = False
+
+        # For contributors functions
         self.created_project_contributors = []
         self.template_schema_contributors = 'path-to-schema'
+
+    # For contributors functions
 
     def _prepare_project_contributor_data(self, _contributor_object, _index, verbose=True):
         pass
@@ -56,4 +201,7 @@ class GRDMClientContributorsCreateFactory(CommonCLIFactory):
         pass
 
     def _overwrite_project_contributors(self, contributors, pk, contributor_user_ids, current_user_contributor):
+        pass
+
+    def _clear_project_current_contributors(self, pk, contributor_user_ids, current_user_contributor, verbose=True):
         pass
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,24 @@
 import argparse
+import logging
 import sys
 from unittest import mock
 
 import pytest
 
-from grdmcli.__main__ import (_add_subparser, _subparser_add_config_args, main)
+from grdmcli.__main__ import (
+    _add_subparser,
+    _subparser_add_config_args,
+    main
+)
 from grdmcli.grdm_client import GRDMClient
+from tests.utils import *
+
+
+@pytest.fixture(autouse=True)
+def set_log_level(caplog):
+    caplog.clear()
+    caplog.set_level(logging.DEBUG)
+
 
 __version__ = '0.0.1.dev0'
 
@@ -56,67 +69,102 @@ def test_subparser_add_config_args__success():
     args = parser.parse_args(["--osf_token", "osf_token", "--osf_api_url", "osf_api_url"])
     assert args.osf_token == 'osf_token'
     assert args.osf_api_url == 'osf_api_url'
+    assert not args.enable_debug
+    assert not args.enable_verbose
+    assert not args.disable_ssl_verify
 
 
 @mock.patch('six.PY2', True)
 @mock.patch('sys.argv', ['grdmcli'])
-def test_main__py2_and_arg_is_one(capfd):
+def test_main__py2_and_arg_is_one(caplog):
     with mock.patch.object(GRDMClient, '__init__', return_value=None):
         main()
-        captured = capfd.readouterr()
-        assert captured.out.__contains__(f'Python2 argparse exits with an error when no command is given')
+    assert len(caplog.records) == 1
+    assert caplog.records[0].levelname == warning_level_log
+    assert caplog.records[0].message.__contains__(f'Python2 argparse exits with an error when no command is given')
 
 
 @mock.patch('six.PY2', False)
-def test_main__exit_code_missing_api_url(monkeypatch, capfd):
+def test_main__exit_code_missing_api_url(monkeypatch, caplog):
     monkeypatch.setattr(sys, 'argv', ['grdmcli', _create, 'create', '--template', 'template-test'])
     exit_code = 'Missing API URL'
-    with mock.patch.object(GRDMClient, f'{_create}_create', side_effect=SystemExit(exit_code)):
-        with pytest.raises(SystemExit) as ex_info:
-            main()
-        assert ex_info.value.code == exit_code
-        captured = capfd.readouterr()
-        lines = captured.out.split('\n')
-        assert lines[0] == f'Start process'
-        assert lines[1] == f'End process'
-        assert lines[2] == f'For help: grdmcli {_create} create --help'
-        assert len(lines) == 4
-        assert captured.err.__contains__(f'ERROR: {exit_code}')
+    with pytest.raises(SystemExit) as ex_info:
+        with mock.patch('grdmcli.grdm_client.common.os.path.exists', return_value=False):
+            with mock.patch.object(GRDMClient, f'{_create}_create', side_effect=SystemExit(exit_code)):
+                main()
+    assert ex_info.value.code == exit_code
+    assert len(caplog.records) == 5
+    assert 'Missing the config file' in caplog.records[0].message
+    assert caplog.records[1].message == 'Start process'
+    assert caplog.records[2].message == 'End process'
+    assert caplog.records[3].message == 'Missing API URL'
+    assert caplog.records[4].message == f'For help: grdmcli {_create} create --help'
 
 
 @mock.patch('six.PY2', False)
-def test_main__exit_code_keyboard_interrupt(monkeypatch, capfd):
+def test_main__exit_code_keyboard_interrupt(monkeypatch, caplog):
     monkeypatch.setattr(sys, 'argv', ['grdmcli', _create, 'create', '--template', 'template-test'])
     exit_code = 'KeyboardInterrupt'
     with mock.patch.object(GRDMClient, f'{_create}_create', side_effect=KeyboardInterrupt):
-        with pytest.raises(SystemExit) as ex_info:
-            main()
+        with mock.patch('grdmcli.grdm_client.common.os.path.exists', return_value=False):
+            with pytest.raises(SystemExit) as ex_info:
+                main()
         assert ex_info.value.code == exit_code
-        captured = capfd.readouterr()
-        lines = captured.out.split('\n')
-        assert lines[0] == f'Start process'
-        assert lines[1] == f'End process'
-        assert lines[2] == f'For help: grdmcli {_create} create --help'
-        assert captured.err.__contains__(f'ERROR: {exit_code}')
-        assert len(lines) == 4
+        assert len(caplog.records) == 5
+        assert caplog.records[0].levelname == warning_level_log
+        assert 'Missing the config file' in caplog.records[0].message
+        assert caplog.records[1].levelname == info_level_log
+        assert caplog.records[1].message == f'Start process'
+        assert caplog.records[2].levelname == info_level_log
+        assert caplog.records[2].message == f'End process'
+        assert caplog.records[3].levelname == error_level_log
+        assert caplog.records[3].message == f'{exit_code}'
+        assert caplog.records[4].levelname == info_level_log
+        assert caplog.records[4].message == f'For help: grdmcli {_create} create --help'
 
 
 @mock.patch('six.PY2', False)
-def test_main__exit_code_false(monkeypatch, capfd):
+def test_main__exit_code_exception(monkeypatch, caplog):
+    monkeypatch.setattr(sys, 'argv', ['grdmcli', _create, 'create', '--template', 'template-test'])
+    exit_code = 'error'
+    with mock.patch.object(GRDMClient, f'{_create}_create', side_effect=Exception(exit_code)):
+        with mock.patch('grdmcli.grdm_client.common.os.path.exists', return_value=False):
+            with pytest.raises(SystemExit) as ex_info:
+                main()
+        assert str(ex_info.value.code) == exit_code
+        assert len(caplog.records) == 5
+        assert caplog.records[0].levelname == warning_level_log
+        assert 'Missing the config file' in caplog.records[0].message
+        assert caplog.records[1].levelname == info_level_log
+        assert caplog.records[1].message == f'Start process'
+        assert caplog.records[2].levelname == info_level_log
+        assert caplog.records[2].message == f'End process'
+        assert caplog.records[3].levelname == error_level_log
+        assert caplog.records[3].message == f'{exit_code}'
+        assert caplog.records[4].levelname == info_level_log
+        assert caplog.records[4].message == f'For help: grdmcli {_create} create --help'
+
+
+@mock.patch('six.PY2', False)
+def test_main__exit_code_false(monkeypatch, caplog):
     monkeypatch.setattr(sys, 'argv', ['grdmcli', _create, 'create', '--template', 'template-test'])
     with mock.patch.object(GRDMClient, f'{_create}_create', return_value=None):
-        main()
-        lines = capfd.readouterr().out.split('\n')
-        assert lines[0] == f'Start process'
-        assert lines[1] == f'End process'
+        with mock.patch('grdmcli.grdm_client.common.os.path.exists', return_value=False):
+            main()
+    assert len(caplog.records) == 3
+    assert caplog.records[0].levelname == warning_level_log
+    assert 'Missing the config file' in caplog.records[0].message
+    assert caplog.records[1].levelname == info_level_log
+    assert caplog.records[1].message == f'Start process'
+    assert caplog.records[2].levelname == info_level_log
+    assert caplog.records[2].message == f'End process'
 
 
 @mock.patch('six.PY2', False)
 def test_main__argparse_not_set_default_function(monkeypatch, capfd):
     monkeypatch.setattr(sys, 'argv', ['grdmcli', _create, 'create', '--template', 'template-test'])
-    with mock.patch('argparse.ArgumentParser.set_defaults', set_defaults=None):
-        with pytest.raises(SystemExit) as ex_info:
+    with pytest.raises(SystemExit) as ex_info:
+        with mock.patch('argparse.ArgumentParser.set_defaults', set_defaults=None):
             main()
-        lines = capfd.readouterr().out.split('\n')
-        assert lines[0] == f'usage: grdmcli {_create} create [-h] --template TEMPLATE'
-        assert ex_info.value.code == 0
+    assert capfd.readouterr().out.__contains__(f'usage: grdmcli {_create} create [-h] --template TEMPLATE')
+    assert ex_info.value.code == 0
